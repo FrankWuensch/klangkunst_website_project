d8f1063db41e12b193f945ebd912f49de3d25b95
FrankWuensch <wuensch.frank@web.de>
2024-04-12 19:36:32 +0200

    solved issue with blurred background

--- src/menu.js
+++ src/menu.js
@@ -2,14 +2,17 @@ document.addEventListener("DOMContentLoaded", function(){
   const menu = document.getElementById("menu");
   const burger = document.getElementById("burger");
   const content = document.getElementById("content");
+
   if (menu && burger && content) {
     burger.addEventListener("click", function () {
       menu.classList.toggle("hidden");
-      content.classList.toggle("blur-sm");
+
       if (!menu.classList.contains("hidden")) {
         menu.classList.add("flex");
+        content.style.filter = "blur(0.5rem)";
       } else {
         menu.classList.remove("flex");
+        content.style.filter = "none"; // Reset the blur effect
       }
     });
   }
