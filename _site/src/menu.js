7b2080617ceff6710e7c28891927e0698e93a653
FrankWuensch95 <103488558+FrankWuensch95@users.noreply.github.com>
2024-08-21 14:07:33 +0200

    remove deploy workflow

---
+++ _site/src/menu.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const menu = document.getElementById("menu");
+  const burger = document.getElementById("burger");
+  const content = document.getElementById("content");
+
+  if (menu && burger && content) {
+    burger.addEventListener("click", function () {
+      menu.classList.toggle("hidden");
+
+      if (!menu.classList.contains("hidden")) {
+        menu.classList.add("flex");
+        content.style.filter = "blur(0.25rem)";
+      } else {
+        menu.classList.remove("flex");
+        content.style.filter = "none"; // Reset the blur effect
+      }
+    });
+  }
+});
