a297ba34362b9f52609e8e896e42f07ad1e81047
FrankWuensch95 <103488558+FrankWuensch95@users.noreply.github.com>
2024-08-26 17:20:39 +0200

    update active state

--- _site/src/active_elements.js
+++ _site/src/active_elements.js
@@ -5,12 +5,14 @@ document.addEventListener("DOMContentLoaded", function() {
     let actualLink = window.location.href.toString();
     navLinks.forEach(function(navLink) {
         if (navLink.href === actualLink) {
-            navLink.classList.add('active')
+            navLink.classList.add('active');
             navLink.classList.remove('dark:text-white/40');
+        } else {
+            navLink.classList.remove("active");
         }
         console.log(`${actualLink} vs ${homeLink}`);
         if (actualLink === homeLink) {
-            home.classList.add('active')
+            home.classList.add('active');
             home.classList.remove('dark:text-white/40');
         }
     });
