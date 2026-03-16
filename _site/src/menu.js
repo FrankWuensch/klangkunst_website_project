33fc0758f27087bc26486b095ab6599cd8981227
Frank Wünsch <wuensch.frank@web.de>
2026-03-16 09:30:24 +0100

    fix: menu handling

--- _site/src/menu.js
+++ _site/src/menu.js
@@ -1,9 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const menu = document.getElementById("mobile-menu");
   const burger = document.getElementById("burger");
   const content = document.getElementById("content");
+  const images = document.querySelectorAll("img.clickable");
   if (menu && burger && content) {
-    burger.addEventListener("click", function (evt) {
+    burger.addEventListener("click", (evt) => {
       if (menu.classList.contains("opacity-0")) {
         console.log(evt.target);
         openMenu();
@@ -12,20 +13,29 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   }
+  content.addEventListener("click", (evt) => {
+    if (!menu.classList.contains("opacity-0")) {
+      closeMenu();
+    }
+  })
   const openMenu = () => {
     menu.classList.add("flex");
     menu.classList.add("opacity-100");
-    content.classList.add("pointer-events-none");
     menu.classList.remove("opacity-0");
     menu.classList.remove("pointer-events-none");
     content.style.filter = "blur(0.25rem)";
+    images.forEach(image => {
+      image.classList.add("pointer-events-none");
+    })
   };
   const closeMenu = () => {
     menu.classList.remove("flex");
     menu.classList.remove("opacity-100");
-    content.classList.remove("pointer-events-none");
     menu.classList.add("opacity-0");
     menu.classList.add("pointer-events-none");
-    content.style.filter = "none"; // Reset the blur effect
+    content.style.filter = "none";
+    images.forEach(image => {
+      image.classList.remove("pointer-events-none");
+    })
   };
 });
